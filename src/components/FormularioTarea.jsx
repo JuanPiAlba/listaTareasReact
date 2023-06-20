506c9359a896bb516983cd713beb93ef26366db4
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-06-20 09:19:07 -0300

    agregamos states para las tareas, y eventos onchange y onsubmit

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,17 +1,26 @@
 import { Button, Form } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+    const [tarea, setTarea] = useState('');
+    const [tareas, setTareas] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setTareas([...tareas, tarea])
+    }
+
     return (
         <>
-        <Form>
-            <Form.Group className="mb-3 d-flex" controlId="tarea">
-                <Form.Control type="text" placeholder="ingrese una Tarea" />
-                <Button variant="primary">Agregar</Button>
-            </Form.Group>
-        </Form>
-        <ListaTareas></ListaTareas>
-        
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3 d-flex" controlId="tarea">
+                    <Form.Control type="text" placeholder="ingrese una Tarea" onChange={(e) => setTarea(e.target.value)} value={tarea} />
+                    <Button variant="primary" type="submit">Agregar</Button>
+                </Form.Group>
+            </Form>
+            <ListaTareas></ListaTareas>
+
         </>
     );
 };
